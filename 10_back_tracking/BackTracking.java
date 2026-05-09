566bcea2590e59b16fe0c69cf5c3bc81c649a21d
Deep joshi <joshideep972@gmail.com>
2026-05-09 20:18:04 +0530

    all permutation

--- 10_back_tracking/BackTracking.java
+++ 10_back_tracking/BackTracking.java
@@ -31,6 +31,19 @@ public class BackTracking{
         sb.deleteCharAt(sb.length()-1);
         allSubstring(str, i+1, sb);
     }
+
+    public static void allPermutation(String s,StringBuilder sb){
+        if(s.length() == 0){
+            System.out.println(sb.toString());
+            return;
+        }
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+            String newString = s.substring(0,i)+ s.substring(i+1);
+            allPermutation(newString, sb.append(ch));
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
     public static void main(String args[]){
         System.out.println("---------------------- Question 1 -------------------------");
         int arr[] = new int[5];
@@ -38,7 +51,8 @@ public class BackTracking{
         printArray(arr);
         System.out.println("---------------------- Question 2 -------------------------");
         allSubstring("abc",0,new StringBuilder(""));
-        System.out.println("---------------------- Question 1 -------------------------");
+        System.out.println("---------------------- Question 3 -------------------------");
+        allPermutation("abc",new StringBuilder());
         System.out.println("---------------------- Question 1 -------------------------");
         System.out.println("---------------------- Question 1 -------------------------");
         System.out.println("---------------------- Question 1 -------------------------");
