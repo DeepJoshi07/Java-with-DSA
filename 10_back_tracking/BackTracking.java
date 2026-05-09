1f317637ec5d5f79ff9d461e982aa4110c07b91e
Deep joshi <joshideep972@gmail.com>
2026-05-09 19:45:55 +0530

    find all sub strings

--- 10_back_tracking/BackTracking.java
+++ 10_back_tracking/BackTracking.java
@@ -16,12 +16,28 @@ public class BackTracking{
         changeArray(arr, i+1, val+1);
         arr[i] = arr[i] - 2;
     }
+
+    public static void allSubstring(String str,int i,StringBuilder sb){
+        if(i == str.length()){
+            if(sb.length() == 0){
+                System.out.println("Null");
+            }else{
+                System.out.println(sb.toString());
+            }
+            return;
+        }
+        sb.append(str.charAt(i));
+        allSubstring(str, i+1, sb);
+        sb.deleteCharAt(sb.length()-1);
+        allSubstring(str, i+1, sb);
+    }
     public static void main(String args[]){
         System.out.println("---------------------- Question 1 -------------------------");
         int arr[] = new int[5];
         changeArray(arr,0,1);
         printArray(arr);
-        System.out.println("---------------------- Question 1 -------------------------");
+        System.out.println("---------------------- Question 2 -------------------------");
+        allSubstring("abc",0,new StringBuilder(""));
         System.out.println("---------------------- Question 1 -------------------------");
         System.out.println("---------------------- Question 1 -------------------------");
         System.out.println("---------------------- Question 1 -------------------------");
