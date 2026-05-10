46fdf10015130dc61fd46a5b8b05cadaf31dc1d5
Deep joshi <joshideep972@gmail.com>
2026-05-10 18:04:48 +0530

    n queens

--- 10_back_tracking/BackTracking.java
+++ 10_back_tracking/BackTracking.java
@@ -44,6 +44,86 @@ public class BackTracking{
             sb.deleteCharAt(sb.length()-1);
         }
     }
+    
+    public static <T> void printBoard(T board[][]){
+        System.out.println("----------- Board -----------");
+        for(T rows[]:board){
+            for(T cell:rows){
+                System.out.print(cell+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void nQueens(Character board[][],int row){
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+
+        for(int i = 0;i < board[0].length;i++){
+            if(isSafe(board,row,i)){
+                board[row][i] = 'Q';
+                nQueens(board, row+1);
+                board[row][i] = 'X';
+            }
+        }
+    }
+
+    public static boolean isNQueens(Character board[][],int row){
+        if(row == board.length){
+            return true;
+        }
+
+        for(int i = 0;i < board[0].length;i++){
+            if(isSafe(board,row,i)){
+                board[row][i] = 'Q';
+                if(isNQueens(board, row+1)){
+                    return true;
+                };
+                board[row][i] = 'X';
+            }
+        }
+        return false;
+    }
+
+    public static int count = 0;
+    public static void nQueensCount(Character board[][],int row){
+        if(row == board.length){
+            count++;
+            return;
+        }
+
+        for(int i = 0;i < board[0].length;i++){
+            if(isSafe(board,row,i)){
+                board[row][i] = 'Q';
+                nQueensCount(board, row+1);
+                board[row][i] = 'X';
+            }
+        }
+    }
+
+    private static boolean isSafe(Character board[][],int row,int col){
+        for(int i = row-1;i >= 0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        for(int i = row,j = col; i >= 0 && j >= 0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        for(int i = row,j = col; i >= 0 && j < board[0].length;i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String args[]){
         System.out.println("---------------------- Question 1 -------------------------");
         int arr[] = new int[5];
@@ -53,9 +133,33 @@ public class BackTracking{
         allSubstring("abc",0,new StringBuilder(""));
         System.out.println("---------------------- Question 3 -------------------------");
         allPermutation("abc",new StringBuilder());
-        System.out.println("---------------------- Question 1 -------------------------");
-        System.out.println("---------------------- Question 1 -------------------------");
-        System.out.println("---------------------- Question 1 -------------------------");
-        System.out.println("---------------------- Question 1 -------------------------");
+        System.out.println("---------------------- Question 4 -------------------------");
+        Character board[][] = new Character[5][5];
+        for(int i = 0;i < board.length;i++){
+            for(int j = 0;j < board[0].length;j++){
+                board[i][j] = 'X';
+            }
+        }
+        // printBoard(board);
+        nQueens(board, 0);
+        System.out.println("----------------");
+        for(int i = 0;i < board.length;i++){
+            for(int j = 0;j < board[0].length;j++){
+                board[i][j] = 'X';
+            }
+        }
+        System.out.println(isNQueens(board, 0));
+        System.out.println("----------------");
+        for(int i = 0;i < board.length;i++){
+            for(int j = 0;j < board[0].length;j++){
+                board[i][j] = 'X';
+            }
+        }
+        nQueensCount(board, 0);
+        System.out.println("the total way to put n queens on board are : "+count);
+        System.out.println("---------------------- Question 5 -------------------------");
+        System.out.println("---------------------- Question 6 -------------------------");
+        System.out.println("---------------------- Question 7 -------------------------");
+        System.out.println("---------------------- Question 8 -------------------------");
     }
 }
