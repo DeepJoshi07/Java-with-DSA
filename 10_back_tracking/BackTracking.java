7661d5e81d43b304374f0829ee935dcd3cda3105
Deep joshi <joshideep972@gmail.com>
2026-05-11 20:41:07 +0530

    sudoku

--- 10_back_tracking/BackTracking.java
+++ 10_back_tracking/BackTracking.java
@@ -1,87 +1,88 @@
-public class BackTracking{
-    public static void printArray(int arr[]){
-        for(int i = 0;i < arr.length;i++){
-            System.out.print(arr[i]+" ");
+public class BackTracking {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    public static void changeArray(int arr[],int i,int val){
-        if(i == arr.length){
+    public static void changeArray(int arr[], int i, int val) {
+        if (i == arr.length) {
             printArray(arr);
             return;
         }
 
         arr[i] = val;
-        changeArray(arr, i+1, val+1);
+        changeArray(arr, i + 1, val + 1);
         arr[i] = arr[i] - 2;
     }
 
-    public static void allSubstring(String str,int i,StringBuilder sb){
-        if(i == str.length()){
-            if(sb.length() == 0){
+    public static void allSubstring(String str, int i, StringBuilder sb) {
+        if (i == str.length()) {
+            if (sb.length() == 0) {
                 System.out.println("Null");
-            }else{
+            } else {
                 System.out.println(sb.toString());
             }
             return;
         }
         sb.append(str.charAt(i));
-        allSubstring(str, i+1, sb);
-        sb.deleteCharAt(sb.length()-1);
-        allSubstring(str, i+1, sb);
+        allSubstring(str, i + 1, sb);
+        sb.deleteCharAt(sb.length() - 1);
+        allSubstring(str, i + 1, sb);
     }
 
-    public static void allPermutation(String s,StringBuilder sb){
-        if(s.length() == 0){
+    public static void allPermutation(String s, StringBuilder sb) {
+        if (s.length() == 0) {
             System.out.println(sb.toString());
             return;
         }
-        for(int i = 0;i < s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            String newString = s.substring(0,i)+ s.substring(i+1);
+            String newString = s.substring(0, i) + s.substring(i + 1);
             allPermutation(newString, sb.append(ch));
-            sb.deleteCharAt(sb.length()-1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
-    
-    public static <T> void printBoard(T board[][]){
+
+    public static <T> void printBoard(T board[][]) {
         System.out.println("----------- Board -----------");
-        for(T rows[]:board){
-            for(T cell:rows){
-                System.out.print(cell+" ");
+        for (T rows[] : board) {
+            for (T cell : rows) {
+                System.out.print(cell + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    public static void nQueens(Character board[][],int row){
-        if(row == board.length){
+    public static void nQueens(Character board[][], int row) {
+        if (row == board.length) {
             printBoard(board);
             return;
         }
 
-        for(int i = 0;i < board[0].length;i++){
-            if(isSafe(board,row,i)){
+        for (int i = 0; i < board[0].length; i++) {
+            if (isSafe(board, row, i)) {
                 board[row][i] = 'Q';
-                nQueens(board, row+1);
+                nQueens(board, row + 1);
                 board[row][i] = 'X';
             }
         }
     }
 
-    public static boolean isNQueens(Character board[][],int row){
-        if(row == board.length){
+    public static boolean isNQueens(Character board[][], int row) {
+        if (row == board.length) {
             return true;
         }
 
-        for(int i = 0;i < board[0].length;i++){
-            if(isSafe(board,row,i)){
+        for (int i = 0; i < board[0].length; i++) {
+            if (isSafe(board, row, i)) {
                 board[row][i] = 'Q';
-                if(isNQueens(board, row+1)){
+                if (isNQueens(board, row + 1)) {
                     return true;
-                };
+                }
+                ;
                 board[row][i] = 'X';
             }
         }
@@ -89,99 +90,168 @@ public class BackTracking{
     }
 
     public static int count = 0;
-    public static void nQueensCount(Character board[][],int row){
-        if(row == board.length){
+
+    public static void nQueensCount(Character board[][], int row) {
+        if (row == board.length) {
             count++;
             return;
         }
 
-        for(int i = 0;i < board[0].length;i++){
-            if(isSafe(board,row,i)){
+        for (int i = 0; i < board[0].length; i++) {
+            if (isSafe(board, row, i)) {
                 board[row][i] = 'Q';
-                nQueensCount(board, row+1);
+                nQueensCount(board, row + 1);
                 board[row][i] = 'X';
             }
         }
     }
 
-    private static boolean isSafe(Character board[][],int row,int col){
-        for(int i = row-1;i >= 0;i--){
-            if(board[i][col] == 'Q'){
+    private static boolean isSafe(Character board[][], int row, int col) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
                 return false;
             }
         }
 
-        for(int i = row,j = col; i >= 0 && j >= 0;i--,j--){
-            if(board[i][j] == 'Q'){
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
 
-        for(int i = row,j = col; i >= 0 && j < board[0].length;i--,j++){
-            if(board[i][j] == 'Q'){
+        for (int i = row, j = col; i >= 0 && j < board[0].length; i--, j++) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
         return true;
     }
-    
-    public static int gridWays(int n,int m,int i,int j){
-        if(i == n && j == m)return 1;
 
-        if(i > n || j > m)return 0;
+    public static int gridWays(int n, int m, int i, int j) {
+        if (i == n && j == m)
+            return 1;
 
-        int way1 = gridWays(n, m, i+1, j);
-        int way2 = gridWays(n, m, i, j+1);
+        if (i > n || j > m)
+            return 0;
+
+        int way1 = gridWays(n, m, i + 1, j);
+        int way2 = gridWays(n, m, i, j + 1);
 
         return way1 + way2;
     }
 
-    public static int factorial(int n){
-        if(n == 0)return 1;
+    public static int factorial(int n) {
+        if (n == 0)
+            return 1;
 
-        return n * factorial(n-1);
+        return n * factorial(n - 1);
     }
 
-    public static int gridWays2(int n,int m){
-        return factorial(n+m-2) /(factorial(n-1) * factorial(m-1));
+    public static int gridWays2(int n, int m) {
+        return factorial(n + m - 2) / (factorial(n - 1) * factorial(m - 1));
     }
-    public static void main(String args[]){
+
+    public static boolean solveSudoku(Integer sudoku[][], int row, int col) {
+        if (row == 9)
+            return true;
+
+        int nextRow = row, nextCol = col + 1;
+        if (nextCol == sudoku.length) {
+            nextRow++;
+            nextCol = 0;
+        }
+
+        if (sudoku[row][col] != 0) {
+            return solveSudoku(sudoku, nextRow, nextCol);
+        }
+
+        for (int i = 1; i <= sudoku.length; i++) {
+            if (isSafePlace(sudoku, row, col, i)) {
+                sudoku[row][col] = i;
+                if (solveSudoku(sudoku, nextRow, nextCol))
+                    return true;
+                sudoku[row][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isSafePlace(Integer sudoku[][], int row, int col, int digit) {
+        for (int i = 0; i < sudoku.length; i++) {
+            if (sudoku[row][i] == digit) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < sudoku.length; i++) {
+            if (sudoku[i][col] == digit) {
+                return false;
+            }
+        }
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
         System.out.println("---------------------- Question 1 -------------------------");
         int arr[] = new int[5];
-        changeArray(arr,0,1);
+        changeArray(arr, 0, 1);
         printArray(arr);
         System.out.println("---------------------- Question 2 -------------------------");
-        allSubstring("abc",0,new StringBuilder(""));
+        allSubstring("abc", 0, new StringBuilder(""));
         System.out.println("---------------------- Question 3 -------------------------");
-        allPermutation("abc",new StringBuilder());
+        allPermutation("abc", new StringBuilder());
         System.out.println("---------------------- Question 4 -------------------------");
         Character board[][] = new Character[5][5];
-        for(int i = 0;i < board.length;i++){
-            for(int j = 0;j < board[0].length;j++){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 board[i][j] = 'X';
             }
         }
         // printBoard(board);
         nQueens(board, 0);
         System.out.println("----------------");
-        for(int i = 0;i < board.length;i++){
-            for(int j = 0;j < board[0].length;j++){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 board[i][j] = 'X';
             }
         }
         System.out.println(isNQueens(board, 0));
         System.out.println("----------------");
-        for(int i = 0;i < board.length;i++){
-            for(int j = 0;j < board[0].length;j++){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 board[i][j] = 'X';
             }
         }
         nQueensCount(board, 0);
-        System.out.println("the total way to put n queens on board are : "+count);
+        System.out.println("the total way to put n queens on board are : " + count);
         System.out.println("---------------------- Question 5 -------------------------");
         System.out.println(gridWays(4, 4, 1, 1));
         System.out.println(gridWays2(4, 4));
         System.out.println("---------------------- Question 6 -------------------------");
+        Integer[][] sudoku = {
+                { 9, 0, 0, 5, 0, 8, 0, 0, 7 },
+                { 0, 8, 0, 3, 0, 2, 9, 0, 5 },
+                { 0, 5, 4, 0, 0, 0, 0, 8, 0 },
+                { 0, 7, 0, 6, 8, 0, 0, 3, 2 },
+                { 1, 0, 0, 0, 0, 4, 0, 0, 8 },
+                { 5, 0, 0, 2, 1, 9, 0, 6, 0 },
+                { 0, 0, 0, 9, 0, 6, 0, 0, 1 },
+                { 7, 2, 6, 0, 0, 1, 0, 4, 0 },
+                { 0, 0, 1, 4, 7, 0, 0, 5, 6 }
+        };
+        System.out.println(solveSudoku(sudoku, 0, 0));
+        printBoard(sudoku);
         System.out.println("---------------------- Question 7 -------------------------");
         System.out.println("---------------------- Question 8 -------------------------");
     }
