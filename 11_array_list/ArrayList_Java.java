1517e983b1aee13c3dddcb1cd5343cc42d8ae630
Deep joshi <joshideep972@gmail.com>
2026-05-14 19:54:45 +0530

    maximum water in container (two pointer approch)

--- 11_array_list/ArrayList_Java.java
+++ 11_array_list/ArrayList_Java.java
@@ -36,6 +36,26 @@ public class ArrayList_Java{
         al.set(idx1,al.get(idx2));
         al.set(idx2,temp);
     }
+
+    public static int maxWaterInContainer(ArrayList<Integer>height){
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size()-1;
+
+        while(lp < rp){
+            int he = Math.min(height.get(rp), height.get(lp));
+            int wi = rp - lp;
+            int currWater = he * wi;
+            maxWater = Math.max(maxWater, currWater);
+
+            if(lp < rp){
+                lp++;
+            }else{
+                rp++;
+            }
+        }
+        return maxWater;
+    }
     public static void main(String[] args) {
         // can only initialized with non primitive values like... Integer,String,Boolean
         // part of java collection fram work
@@ -125,5 +145,18 @@ public class ArrayList_Java{
         System.out.println("--------------------------");
 
         // ----------------------------------------------------------
+
+        System.out.println("--------------------- Question 1 -----------------------");
+        ArrayList<Integer> height =  new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(maxWaterInContainer(height));
     }
 }
