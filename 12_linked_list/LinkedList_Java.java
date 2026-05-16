6571a96344affe240ac1166e5c8818df331822ff
Deep joshi <joshideep972@gmail.com>
2026-05-16 16:34:57 +0530

    creating node class,head and tail.

---
+++ 12_linked_list/LinkedList_Java.java
@@ -0,0 +1,19 @@
+
+
+public class LinkedList_Java {
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static void main(String[] args) {
+        
+    }
+}
