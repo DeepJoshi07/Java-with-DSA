1639f7a64a8b97c83bfe8f226f2da897f14a31ce
Deep joshi <joshideep972@gmail.com>
2026-05-23 19:53:40 +0530

    next right greater using stack

--- 13_stack/Stack_Java.java
+++ 13_stack/Stack_Java.java
@@ -48,6 +48,25 @@ public class Stack_Java {
             s.push(i);
         }
     }
+
+    public static int[] nextRightGreater(int arr[]){
+        Stack<Integer> s = new Stack<>();
+        int nextGreater [] = new int[arr.length];
+
+        for(int i = arr.length-1;i >= 0;i--){
+            int curr = arr[i];
+            while(!s.isEmpty() && arr[s.peek()] < curr){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextGreater[i] = -1;
+            }else{
+                nextGreater[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+        return nextGreater;
+    }
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
@@ -92,6 +111,12 @@ public class Stack_Java {
         }
         System.out.println();
         System.out.println("------------------ Question 5 ---------------------");
+        int numbers [] = {6,8,0,1,3};
+        int next_Right_Greater [] = nextRightGreater(numbers);
+        for(int i = 0;i < next_Right_Greater.length;i++){
+            System.out.print(next_Right_Greater[i]+" ");
+        }
+        System.out.println();
         System.out.println("------------------ Question 6 ---------------------");
         System.out.println("------------------ Question 7 ---------------------");
     }
