ea71ea60e9b6e5a9d0fec101024e4e68f58bc819
Deep joshi <joshideep972@gmail.com>
2026-05-31 15:32:38 +0530

    Queue using Deque

--- 14_queue/deque/Queue_Deque_Java.java
+++ 14_queue/deque/Queue_Deque_Java.java
@@ -1,5 +1,33 @@
-package 14_queue.deque;
+import java.util.*;
 
-public class Deque_Java {
-    
+public class Queue_Deque_Java {
+    Deque<Integer> dq = new LinkedList<>();
+
+    public boolean isEmpty(){
+        return dq.isEmpty();
+    }
+
+    public void add(int data){
+        dq.addLast(data);
+    }
+
+    public int remove(){
+        return dq.removeFirst();
+    }
+
+    public int peek(){
+        return dq.getFirst();
+    }
+    public static void main(String[] args) {
+        Queue_Deque_Java dqj = new Queue_Deque_Java();
+        dqj.add(1);
+        dqj.add(2);
+        dqj.add(3);
+        dqj.add(4);
+        dqj.add(5);
+        System.out.println(dqj.peek());
+        while(!dqj.isEmpty()){
+            System.out.println(dqj.remove());
+        }
+    }
 }
