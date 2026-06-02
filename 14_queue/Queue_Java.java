13c7fcbc1c31987ee0d17792e704845877cf3286
DeepJoshi07 <joshideep972@gmail.com>
2026-06-02 19:00:00 +0530

    Reverse a Queue till Nth Element.

--- 14_queue/Queue_Java.java
+++ 14_queue/Queue_Java.java
@@ -125,6 +125,23 @@ public class Queue_Java {
         }
         System.out.println();
     }
+
+    public static void reverseQueueTillNthElement(Queue<Integer>q,int n){
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0;i < n;i++){
+            s.push(q.remove());
+        }
+
+        int size = q.size();
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+        
+        for(int i = 0;i < size;i++){
+            q.add(q.remove());
+        }
+        System.out.println(q);
+    }
     public static void main(String[] args) {
         System.out.println("---------------------- Question 1 --------------------------");
         String str1 = "aabccxb";
@@ -186,5 +203,17 @@ public class Queue_Java {
         al.add (new Job("d",1,30));
         jobSequence(al);
         System.out.println("---------------------- Question 8 --------------------------");
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        q.add(60);
+        q.add(70);
+        q.add(80);
+        q.add(90);
+        q.add(100);
+        reverseQueueTillNthElement(q,5);
     }
 }
