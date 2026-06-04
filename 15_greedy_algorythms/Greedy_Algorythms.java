456ccc55d0bab926bc3df279a800ab69d15801a7
DeepJoshi07 <joshideep972@gmail.com>
2026-06-04 19:22:31 +0530

    fractional knapsack

--- 15_greedy_algorythms/Greedy_Algorythms.java
+++ 15_greedy_algorythms/Greedy_Algorythms.java
@@ -30,10 +30,46 @@ public class Greedy_Algorythms {
         System.out.println();
         System.out.println("total activities are = "+ maxActivities);
     }
+
+    public static void fractionalKnapsack(int val[],int weight[],int totalWeight){
+        double ratio[][] = new double[val.length][2];
+        
+        for(int i = 0;i < val.length;i++){
+            ratio[i][0] = i;
+            ratio[i][1] = (double)val[i]/weight[i];
+        }
+
+        Arrays.sort(ratio,Comparator.comparingDouble(o -> o[1]));
+        double maxValue = 0;
+
+        for(int i = val.length-1;i >= 0;i--){
+            int idx = (int)ratio[i][0];
+            if(totalWeight >= weight[idx]){
+                totalWeight -= weight[idx];
+                maxValue += val[idx];
+            }else{
+                maxValue += ratio[i][1] * totalWeight;
+                totalWeight = 0;
+            }
+        }
+        System.out.println("maximum value in knapsack is = "+maxValue);
+    }
     public static void main(String[] args) {
         System.out.println("----------------- Question 1 ---------------");
         int start[] = {1,3,0,5,8,5};
         int end[] = {2,4,6,7,9,9};
         activitySelection(start,end);
+        System.out.println("----------------- Question 2 ---------------");
+        int val[] = {60,100,120};
+        int weight[] = {10,20,30};
+        fractionalKnapsack(val,weight,50);
+        System.out.println("----------------- Question 3 ---------------");
+        System.out.println("----------------- Question 4 ---------------");
+        System.out.println("----------------- Question 5 ---------------");
+        System.out.println("----------------- Question 6 ---------------");
+        System.out.println("----------------- Question 7 ---------------");
+        System.out.println("----------------- Question 8 ---------------");
+        System.out.println("----------------- Question 9 ---------------");
+        System.out.println("----------------- Question 10 ---------------");
     }
 }
