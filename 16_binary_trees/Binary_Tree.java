8ed9f6349aa43cba368f7845fe89b2a89063b176
DeepJoshi07 <joshideep972@gmail.com>
2026-06-10 17:38:59 +0530

    Pre-Order

--- 16_binary_trees/Binary_Tree.java
+++ 16_binary_trees/Binary_Tree.java
@@ -25,12 +25,22 @@ public class Binary_Tree {
 
         return newNode;
     }
+
+    public static void preOrder(Node root){
+        if(root == null)return;
+
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
     public static void main(String[] args) {
         System.out.println("-------------------- Question 1 -------------------");
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = buildTree(nodes);
         System.out.println(root.data);
         System.out.println("-------------------- Question 2 -------------------");
+        preOrder(root);
+        System.out.println();
         System.out.println("-------------------- Question 3 -------------------");
         System.out.println("-------------------- Question 4 -------------------");
         System.out.println("-------------------- Question 5 -------------------");
