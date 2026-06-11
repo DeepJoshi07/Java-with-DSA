35cfa3117500a4a0b4be8f7e7eaebf0901b4c672
DeepJoshi07 <joshideep972@gmail.com>
2026-06-11 17:29:19 +0530

    subtree of another tree

--- 16_binary_trees/Binary_Tree.java
+++ 16_binary_trees/Binary_Tree.java
@@ -3,11 +3,11 @@ import java.util.Queue;
 
 public class Binary_Tree {
 
-    static class Node{
+    static class Node {
         int data;
-        Node right,left;
+        Node right, left;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.right = null;
             this.left = null;
@@ -15,9 +15,10 @@ public class Binary_Tree {
     }
 
     static int idx = -1;
-    public static Node buildTree(int nodes[]){
+
+    public static Node buildTree(int nodes[]) {
         idx++;
-        if(nodes[idx] == -1){
+        if (nodes[idx] == -1) {
             return null;
         }
 
@@ -28,118 +29,155 @@ public class Binary_Tree {
         return newNode;
     }
 
-    public static void preOrder(Node root){
-        if(root == null)return;
+    public static void preOrder(Node root) {
+        if (root == null)
+            return;
 
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
-    public static void inOrder(Node root){
-        if(root == null)return;
+    public static void inOrder(Node root) {
+        if (root == null)
+            return;
 
         inOrder(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         inOrder(root.right);
     }
 
-    public static void postOrder(Node root){
-        if(root == null)return;
+    public static void postOrder(Node root) {
+        if (root == null)
+            return;
 
         postOrder(root.left);
         postOrder(root.right);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
     }
-    
-    public static void levelOrder(Node root){
+
+    public static void levelOrder(Node root) {
         Queue<Node> q = new LinkedList<>();
         q.add(root);
         q.add(null);
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Node curr = q.remove();
-            if(curr == null){
+            if (curr == null) {
                 System.out.println();
-                if(q.isEmpty()){
+                if (q.isEmpty()) {
                     break;
-                }else{
+                } else {
                     q.add(null);
                 }
-            }else{
-                System.out.print(curr.data+" ");
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
+            } else {
+                System.out.print(curr.data + " ");
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
             }
         }
     }
 
-    public static int height(Node root){
-        if(root == null)return 0;
+    public static int height(Node root) {
+        if (root == null)
+            return 0;
 
         int left = height(root.left);
         int right = height(root.right);
 
         return Math.max(left, right) + 1;
     }
-   
-    public static int countOfNodes(Node root){
-        if(root == null)return 0;
+
+    public static int countOfNodes(Node root) {
+        if (root == null)
+            return 0;
 
         int left = countOfNodes(root.left);
         int right = countOfNodes(root.right);
 
         return left + right + 1;
     }
-    
-    public static int sumOfTree(Node root){
-        if(root == null)return 0;
+
+    public static int sumOfTree(Node root) {
+        if (root == null)
+            return 0;
 
         int left = sumOfTree(root.left);
         int right = sumOfTree(root.right);
 
         return left + right + root.data;
     }
-   
-    public static int diameterOfTree(Node root){
-        if(root == null)return 0;
+
+    public static int diameterOfTree(Node root) {
+        if (root == null)
+            return 0;
 
         int leftDiameter = diameterOfTree(root.left);
         int rightDiameter = diameterOfTree(root.right);
         int left = height(root.left);
-        int right =  height(root.right);
+        int right = height(root.right);
 
         int selfDiameter = left + right + 1;
 
-        return Math.max(selfDiameter, Math.max(rightDiameter,leftDiameter));
+        return Math.max(selfDiameter, Math.max(rightDiameter, leftDiameter));
     }
-    
-    static class DiameterInfo{
+
+    static class DiameterInfo {
         int height;
         int diameter;
 
-        public DiameterInfo(int height,int diameter){
+        public DiameterInfo(int height, int diameter) {
             this.diameter = diameter;
             this.height = height;
         }
     }
 
-    public static DiameterInfo diameterOfTree2(Node root){
-        if(root == null){
+    public static DiameterInfo diameterOfTree2(Node root) {
+        if (root == null) {
             return new DiameterInfo(0, 0);
         }
 
         DiameterInfo left = diameterOfTree2(root.left);
         DiameterInfo right = diameterOfTree2(root.right);
 
-        int selfDiameter = Math.max(Math.max(left.diameter, right.diameter),left.height+right.height+1);
-        int height = Math.max(left.height, right.height)+1 ;
+        int selfDiameter = Math.max(Math.max(left.diameter, right.diameter), left.height + right.height + 1);
+        int height = Math.max(left.height, right.height) + 1;
 
         return new DiameterInfo(height, selfDiameter);
     }
+
+    private static boolean isIdentical(Node node, Node subRoot) {
+        if (node == null && subRoot == null) {
+            return true;
+        } else if (node == null || subRoot == null) {
+            return false;
+        } else if (node.data != subRoot.data) {
+            return false;
+        }
+
+        return isIdentical(node.left, subRoot.left) && isIdentical(node.right, subRoot.right);
+
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null)
+            return false;
+
+        if (root.data == subRoot.data) {
+            return isIdentical(root, subRoot);
+        }
+
+        boolean left = isSubtree(root.left, subRoot);
+        boolean right = isSubtree(root.right, subRoot);
+
+        return left || right;
+    }
+
     public static void main(String[] args) {
         System.out.println("-------------------- Question 1 -------------------");
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Node root = buildTree(nodes);
         System.out.println(root.data);
         System.out.println("-------------------- Question 2 -------------------");
@@ -160,8 +198,17 @@ public class Binary_Tree {
         System.out.println("-------------------- Question 8 -------------------");
         System.out.println(sumOfTree(root));
         System.out.println("-------------------- Question 9 -------------------");
-        System.out.println("Diameter approch 1 :"+diameterOfTree(root));
+        System.out.println("Diameter approch 1 :" + diameterOfTree(root));
         System.out.println("-------------------- Question 10 -------------------");
-        System.out.println("Diameter approch 2 :"+diameterOfTree2(root).diameter);
+        System.out.println("Diameter approch 2 :" + diameterOfTree2(root).diameter);
+        System.out.println("-------------------- Question 11 -------------------");
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+        System.out.println(isSubtree(root, subRoot));
+        System.out.println("-------------------- Question 12 -------------------");
+        System.out.println("-------------------- Question 13 -------------------");
+        System.out.println("-------------------- Question 14 -------------------");
+        System.out.println("-------------------- Question 15 -------------------");
     }
 }
