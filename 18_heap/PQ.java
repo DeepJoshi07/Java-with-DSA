d76df7e827baa17343326c1ac8c9c96eea519b13
DeepJoshi07 <joshideep972@gmail.com>
2026-06-21 14:20:00 +0530

    JCF Priority Queue with objects

--- 18_heap/PQ.java
+++ 18_heap/PQ.java
@@ -2,7 +2,21 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PQ {
-   
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s) {
+            return this.rank - s.rank;
+        }
+    }
 
     public static void main(String[] args) {
         PriorityQueue<Integer> pq1 = new PriorityQueue<>();
@@ -25,6 +39,18 @@ public class PQ {
 
         while (!pq2.isEmpty()) {
             System.out.println(pq2.remove());
+        }
+        System.out.println("-------------- with objects ------------------");
+        PriorityQueue<Student> pq3 = new PriorityQueue<>();
+        pq3.add(new Student("deep", 5));
+        pq3.add(new Student("harman", 2));
+        pq3.add(new Student("jacob", 4));
+        pq3.add(new Student("jimmy", 3));
+        pq3.add(new Student("roman", 1));
+
+        while (!pq3.isEmpty()) {
+            System.out.println(pq3.peek().name + ", " + pq3.peek().rank);
+            pq3.remove();
         }
     }
 }
