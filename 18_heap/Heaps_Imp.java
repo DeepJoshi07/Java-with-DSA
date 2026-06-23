86b647886bb1991b21477b7ea3ee23c329ab36b7
DeepJoshi07 <joshideep972@gmail.com>
2026-06-23 14:20:00 +0530

    Heap Sort

--- 18_heap/Heaps_Imp.java
+++ 18_heap/Heaps_Imp.java
@@ -59,6 +59,47 @@ public class Heaps_Imp {
     public boolean isEmpty(){
         return al.size() == 0;
     }
+    
+    private static void heapify2(int arr[],int i,int ei){
+        int left = i*2 + 1;
+        int right = i*2 + 2;
+        int maxIdx = i;
+
+        if(left < ei && arr[left] > arr[maxIdx]) maxIdx = left;
+        if(right < ei && arr[right] > arr[maxIdx]) maxIdx = right;
+
+        if(maxIdx != i){
+            int temp = arr[maxIdx];
+            arr[maxIdx] = arr[i];
+            arr[i] = temp;
+
+            heapify2(arr, maxIdx, ei);
+        }
+
+    }
+
+    public static void heapSort(int arr[]){
+
+        int n = arr.length;
+        for(int i = n/2 -1;i >= 0;i--){
+            heapify2(arr, i, n);
+        }
+
+        for(int i = n-1;i > 0;i--){
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+            
+            heapify2(arr, 0, i);
+        }
+    }
+    
+    public static void printArr(int arr[]){
+        for(int i = 0;i < arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Heaps_Imp hpm = new Heaps_Imp();
         hpm.insert(6);
@@ -70,5 +111,9 @@ public class Heaps_Imp {
         while(!hpm.isEmpty()){
             System.out.println(hpm.remove());
         }
+        System.out.println("-------------------- heapSort ----------------------");
+        int arr[] = {5,2,1,4,3};
+        heapSort(arr);
+        printArr(arr);
     }
 }
