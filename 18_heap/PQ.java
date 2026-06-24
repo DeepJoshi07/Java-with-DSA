2130c0b01cd931892a93ad87e991708455aa04bf
DeepJoshi07 <joshideep972@gmail.com>
2026-06-24 14:20:00 +0530

    Nearby Cars

--- 18_heap/PQ.java
+++ 18_heap/PQ.java
@@ -18,6 +18,34 @@ public class PQ {
         }
     }
 
+    static class CarInfo implements Comparable<CarInfo>{
+        int x,y,idx;
+        int distenceSquare;
+
+        public CarInfo(int idx,int x,int y,int ds){
+            this.idx = idx;
+            this.x = x;
+            this.y = y;
+            this.distenceSquare = ds;
+        }
+
+        @Override
+        public int compareTo(CarInfo c){
+            return this.distenceSquare - c.distenceSquare;
+        }
+    }
+    public static void kNearestCars(int arr[][],int k){
+        PriorityQueue<CarInfo> pq = new PriorityQueue<>();
+        for(int i = 0;i < arr.length;i++){
+            int distSq = arr[i][0] * arr[i][0] + arr[i][1] * arr[i][1];
+            pq.add(new CarInfo(i,arr[i][0], arr[i][1], distSq));
+        }
+
+        for(int i = 0;i < k;i++){
+            System.out.println("C"+pq.remove().idx);
+        }
+    }
+
     public static void main(String[] args) {
         PriorityQueue<Integer> pq1 = new PriorityQueue<>();
         pq1.add(7);
@@ -52,5 +80,13 @@ public class PQ {
             System.out.println(pq3.peek().name + ", " + pq3.peek().rank);
             pq3.remove();
         }
+        System.out.println("----------------------- Question 1 ---------------------------");
+        int pts[][] = {{3,3},{5,-1,},{-2,4}};
+        int k = 2;
+        kNearestCars(pts,k);
+        System.out.println("----------------------- Question 2 ---------------------------");
+        System.out.println("----------------------- Question 3 ---------------------------");
+        System.out.println("----------------------- Question 4 ---------------------------");
+        System.out.println("----------------------- Question 5 ---------------------------");
     }
 }
