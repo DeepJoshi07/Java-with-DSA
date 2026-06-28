af2689000aad8c5634b15f5e1d269a7dda433a05
DeepJoshi07 <joshideep972@gmail.com>
2026-06-28 14:13:00 +0530

    Get all kth number

--- 18_heap/PQ.java
+++ 18_heap/PQ.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class PQ {
@@ -18,11 +20,11 @@ public class PQ {
         }
     }
 
-    static class CarInfo implements Comparable<CarInfo>{
-        int x,y,idx;
+    static class CarInfo implements Comparable<CarInfo> {
+        int x, y, idx;
         int distenceSquare;
 
-        public CarInfo(int idx,int x,int y,int ds){
+        public CarInfo(int idx, int x, int y, int ds) {
             this.idx = idx;
             this.x = x;
             this.y = y;
@@ -30,100 +32,127 @@ public class PQ {
         }
 
         @Override
-        public int compareTo(CarInfo c){
+        public int compareTo(CarInfo c) {
             return this.distenceSquare - c.distenceSquare;
         }
     }
-    public static void kNearestCars(int arr[][],int k){
+
+    public static void kNearestCars(int arr[][], int k) {
         PriorityQueue<CarInfo> pq = new PriorityQueue<>();
-        for(int i = 0;i < arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             int distSq = arr[i][0] * arr[i][0] + arr[i][1] * arr[i][1];
-            pq.add(new CarInfo(i,arr[i][0], arr[i][1], distSq));
+            pq.add(new CarInfo(i, arr[i][0], arr[i][1], distSq));
         }
 
-        for(int i = 0;i < k;i++){
-            System.out.println("C"+pq.remove().idx);
+        for (int i = 0; i < k; i++) {
+            System.out.println("C" + pq.remove().idx);
         }
     }
 
-    public static void connectNRopes(int ropes[]){
+    public static void connectNRopes(int ropes[]) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0;i < ropes.length;i++){
+        for (int i = 0; i < ropes.length; i++) {
             pq.add(ropes[i]);
         }
 
         int cost = 0;
-        while(pq.size() > 1){
+        while (pq.size() > 1) {
             int c1 = pq.remove();
             int c2 = pq.remove();
             cost += c1 + c2;
-            pq.add(c1+c2);
+            pq.add(c1 + c2);
         }
-        System.out.println("minimum cost to connect n ropes is : "+cost);
+        System.out.println("minimum cost to connect n ropes is : " + cost);
     }
 
-    static class ArmyRow implements Comparable<ArmyRow>{
-        int soldiers,idx;
-        public ArmyRow(int soldiers,int idx){
+    static class ArmyRow implements Comparable<ArmyRow> {
+        int soldiers, idx;
+
+        public ArmyRow(int soldiers, int idx) {
             this.idx = idx;
             this.soldiers = soldiers;
         }
 
         @Override
-        public int compareTo(ArmyRow a){
-            if(this.soldiers == a.soldiers){
+        public int compareTo(ArmyRow a) {
+            if (this.soldiers == a.soldiers) {
                 return this.idx - a.idx;
             }
             return this.soldiers - a.soldiers;
         }
     }
 
-    public static void weakestSoldierRow(int arr[][]){
+    public static void weakestSoldierRow(int arr[][]) {
         PriorityQueue<ArmyRow> pq = new PriorityQueue<>();
-        for(int i = 0;i < arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             int count = 0;
-            for(int j = 0;j < arr[0].length;j++){
-                count += arr[i][j] == 1? 1:0;
+            for (int j = 0; j < arr[0].length; j++) {
+                count += arr[i][j] == 1 ? 1 : 0;
             }
             pq.add(new ArmyRow(count, i));
         }
 
-        while(!pq.isEmpty()){
-            System.out.print("S"+pq.remove().idx+" ");
+        while (!pq.isEmpty()) {
+            System.out.print("S" + pq.remove().idx + " ");
         }
         System.out.println();
     }
 
-    static class Pair implements Comparable<Pair>{
-        int val,idx;
-        public Pair(int val,int idx){
+    static class Pair implements Comparable<Pair> {
+        int val, idx;
+
+        public Pair(int val, int idx) {
             this.idx = idx;
             this.val = val;
         }
 
         @Override
-        public int compareTo(Pair p){
+        public int compareTo(Pair p) {
             return p.val - this.val;
         }
     }
 
-    public static void slidingWindowMaximum(int nums[],int k){
+    public static void slidingWindowMaximum(int nums[], int k) {
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for(int i = 0;i < k;i++){
+        for (int i = 0; i < k; i++) {
             pq.add(new Pair(nums[i], i));
         }
 
-        System.out.print(pq.peek().val+" ");
+        System.out.print(pq.peek().val + " ");
 
-        for(int i = k;i < nums.length;i++){
-            while(!pq.isEmpty() && pq.peek().idx <= i - k){
+        for (int i = k; i < nums.length; i++) {
+            while (!pq.isEmpty() && pq.peek().idx <= i - k) {
                 pq.remove();
             }
             pq.add(new Pair(nums[i], i));
-            System.out.print(pq.peek().val+" ");
+            System.out.print(pq.peek().val + " ");
         }
         System.out.println();
     }
+
+    public static PriorityQueue<Integer> min;
+    public static int kTh;
+
+    public static List<Integer> getAllKthNumber(int arr[]) {
+        List<Integer> list = new ArrayList<>();
+        for (int val : arr) {
+            if (min.size() < kTh)
+                min.add(val);
+            else {
+                if (val > min.peek()) {
+                    min.poll();
+                    min.add(val);
+                }
+            }
+            if (min.size() < kTh)
+                list.add(-1);
+            else
+                list.add(min.peek());
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
         PriorityQueue<Integer> pq1 = new PriorityQueue<>();
         pq1.add(7);
@@ -159,24 +188,35 @@ public class PQ {
             pq3.remove();
         }
         System.out.println("----------------------- Question 1 ---------------------------");
-        int pts[][] = {{3,3},{5,-1,},{-2,4}};
+        int pts[][] = { { 3, 3 }, { 5, -1, }, { -2, 4 } };
         int k = 2;
-        kNearestCars(pts,k);
+        kNearestCars(pts, k);
         System.out.println("----------------------- Question 2 ---------------------------");
-        int ropes[] = {4,3,2,6};
+        int ropes[] = { 4, 3, 2, 6 };
         connectNRopes(ropes);
         System.out.println("----------------------- Question 3 ---------------------------");
-        int army [][] = {
-            {1,0,0,0},
-            {1,1,1,1},
-            {1,0,0,0},
-            {1,0,0,0},
+        int army[][] = {
+                { 1, 0, 0, 0 },
+                { 1, 1, 1, 1 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 0 },
         };
         weakestSoldierRow(army);
         System.out.println("----------------------- Question 4 ---------------------------");
-        int numbers[] = {1,3,-1,-3,5,3,6,7};
+        int numbers[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
         int size = 3;
-        slidingWindowMaximum(numbers,size);
+        slidingWindowMaximum(numbers, size);
         System.out.println("----------------------- Question 5 ---------------------------");
+        min = new PriorityQueue<>();
+        kTh = 4;
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        List<Integer> res = getAllKthNumber(arr);
+        for (int x : res)
+            System.out.print(x + " ");
+        System.out.println();
+        System.out.println("----------------------- Question 6 ---------------------------");
+        System.out.println("----------------------- Question 7 ---------------------------");
+        System.out.println("----------------------- Question 8 ---------------------------");
+        System.out.println("----------------------- Question 9 ---------------------------");
     }
 }
